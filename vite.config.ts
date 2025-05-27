9c8be74b2a4040e6861cb192ac2fe1b8a36a97dd
makkenzo <nekgo2009@gmail.com>
2025-05-27 12:47:21 +0500

    test

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "src": path.resolve(__dirname, "./src"),
-    },
-  },
-  preview: {
-    host: true,
-    port: 3000,
-    // allowedHosts: ['services-frontend.anapp.kz']
-  }
-})
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+
+    return {
+        plugins: [react()],
+        resolve: {
+            alias: {
+                src: path.resolve(__dirname, './src'),
+            },
+        },
+        preview: {
+            host: true,
+            port: 3000,
+            // allowedHosts: ['services-frontend.anapp.kz']
+        },
+    };
+});
